97e70567d18b723e7d2be1189a28af941e4d91a1
claramarsango <claraxmarsango@gmail.com>
2023-02-22 10:35:18 +0100

    fix api fetch protocol to https

--- src/features/random/randomNumberAPI.ts
+++ src/features/random/randomNumberAPI.ts
@@ -1,6 +1,6 @@
 const getRandomNumber = async () => {
   const response = await fetch(
-    "http://www.randomnumberapi.com/api/v1.0/random?min=10&max=50"
+    "https://www.randomnumberapi.com/api/v1.0/random?min=10&max=50"
   );
   const number = await response.json();
 
